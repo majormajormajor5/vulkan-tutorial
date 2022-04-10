45f5707fad49f98c0abb3d00b1182445b618901c
Anton <apavl2@softserveinc.com>
2022-04-10 16:34:20 +0300

    04 Success pipeline configuration mainly

--- lve_pipeline.hpp
+++ lve_pipeline.hpp
@@ -7,7 +7,21 @@
 
 namespace lve {
 
-    struct PipelineConfigInfo {};
+    struct PipelineConfigInfo {
+        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+        VkViewport viewport;
+        VkRect2D scissor;
+        VkPipelineViewportStateCreateInfo viewportInfo;
+        VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+        VkPipelineMultisampleStateCreateInfo multisampleInfo;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment;
+        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+        VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+        VkPipelineLayout pipelineLayout = nullptr;
+        VkRenderPass renderPass = nullptr;
+        uint32_t subpass = 0;
+    };
+
     class LvePipeline {
     public:
         LvePipeline(
@@ -16,21 +30,27 @@ namespace lve {
                 const std::string &fragFilepath,
                 const PipelineConfigInfo &config
         );
-        ~LvePipeline() {}
 
-        LvePipeline(const LvePipeline&) = delete;
-        void operator=(const LvePipeline&) = delete;
+        ~LvePipeline();
 
-        static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
+        LvePipeline(const LvePipeline &) = delete;
+
+        void operator=(const LvePipeline &) = delete;
+
+        static PipelineConfigInfo defaultPipelineConfigInfo(int32_t width, int32_t height);
 
     private:
-        static std::vector<char> readFile(const std::string& filePath);
+        static std::vector<char> readFile(const std::string &filePath);
 
-        void createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath);
+        void createGraphicsPipeline(
+                const std::string &vertFilepath,
+                const std::string &fragFilepath,
+                const PipelineConfigInfo &configInfo
+        );
 
-        void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
+        void createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule);
 
-        LveDevice& lveDevice;
+        LveDevice &lveDevice;
         VkPipeline graphicsPipeline;
         VkShaderModule vertShaderModule;
         VkShaderModule fragShaderModule;
